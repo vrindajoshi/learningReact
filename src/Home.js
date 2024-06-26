367cf466ee1b8be44a79ece0e3601ceede7c900c
vrindaj <vrinda.joshi@hotmail.com>
2024-06-26 16:51:22 -0400

    json-server

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
-    // name of var and set method (re-renders name, makes reactive)
-    const [name, setName] = useState("mario");
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e) => {
-        setAge(age+1);
-
+    const [blogs, setBlogs] = useState([
+        {title: "my website", body: "lorem ipsum....", author: "vrinda", id: 1},
+        {title: "update", body: "lorem ipsum....", author: "joe", id: 2},
+        {title: "hi", body: "lorem ipsum....", author: "vrinda", id: 3},
+    ]);
+   
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
+
+    // useEffect is called everytime the page is re-rendered, can be chenged by dependencies
+    useEffect(() => {
+        console.log("use effect ran");
+    }, []);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is {age} years old</p>
-            <button onClick={handleClick}>increase age!</button>
+            <BlogList blogs={blogs} title="blogs" handleDelete={handleDelete}/>
         </div>
     );
 }
